ef8375208a5efd721a6b043ca30c104c5fa4aa53
blackALT <wanessapssouza@gmail.com>
2020-10-24 21:01:46 -0300

    Route

--- projetos/revisao-API-S11/src/routes/musicRoute.js
+++ projetos/revisao-API-S11/src/routes/musicRoute.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const controller = require("../controller/musicController")
 
 router.get("/", controller.getAllMusics)
-router.post("/", controller.createMusic)
-router.get("/artists", controller.getMusicByArtist)
-router.get("/decade", controller.getMusicsAfter2010)
+router.get("/sandy", controller.getMusicBySandy) // Contratos para ir ao infinito e além
+router.get("/decade", controller.getMusicsAfter2010) // Contratos para ir ao infinito e além
+router.get("/favorite", controller.getFavoriteMusics) // Extra
 router.get("/:id", controller.getMusicById)
+router.post("/", controller.createMusic)
 router.put("/:id", controller.updateMusic)
 router.patch("/:id/favorited", controller.updateFavoritedStatus)
 router.delete("/:id", controller.deleteMusic)
